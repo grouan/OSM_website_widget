7aa8c56fdbcb871f6a0c271cdc37a472277aa823
Guillaume Rouan <g.rouan@free.fr>
2016-08-17 15:36:36 +0200

    Update javascript_part.js

--- javascript_part.js
+++ javascript_part.js
@@ -3,7 +3,7 @@
 /* Map Settings */
 
 		var mymap = L.map('my_osm_widget_map', { /* use the same name as your <div id=""> */
-    			center: [XXX-LATITUDE-CENTER-XXX, XXX-LONGITUDE-CENTER-XXX], /* set the center of the displayed map */
+    			center: [XXX-LATITUDE-CENTER-XXX, XXX-LONGITUDE-CENTER-XXX], /* set the center of the displayed map as GPS Coordinates : [LAT,LON] */
     			zoom: 17, /* define the zoom level */
     			zoomControl: false, /* false = no zoom control buttons displayed */
 			scrollWheelZoom: false /* false = scrolling zoom on the map is locked */
